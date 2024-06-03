76fed649a1b3eede29a6d598475235cbbd199cf6
Thomas Bartleet <42772107+GlassesToCSharp@users.noreply.github.com>
2024-06-03 20:03:47 +0100

    Started implementing layout of the Device page

--- lib/features/devices/device/device_page.dart
+++ lib/features/devices/device/device_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:home_app/features/devices/models/device.dart';
 
 export 'package:home_app/features/devices/models/device.dart';
@@ -15,6 +17,54 @@ class DevicePage extends StatefulWidget {
 class _DevicePageState extends State<DevicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final items = [
+      // Title - Device name
+      Row(
+        children: [
+          Expanded(
+            child: Text(
+              widget.device.name,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              // TODO: Display edit box for device name.
+            },
+            icon: const Icon(FontAwesomeIcons.pen),
+          ),
+        ],
+      ),
+      // Power state
+      // Motor control
+      // LED colour
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.device.ipAddress),
+        scrolledUnderElevation: 8,
+        shadowColor: Colors.grey,
+      ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Container(
+          height: 0.5,
+          color: Colors.grey,
+        ),
+        itemCount: items.length + 1, // To include the last separator, add 1.
+        itemBuilder: ((context, index) {
+          if (index >= items.length) {
+            return const SizedBox();
+          }
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: items[index],
+          );
+        }),
+      ),
+    );
   }
 }
